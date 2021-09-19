e5e056b102221e362bc618b454a89e827bf36030
ebfaler <74653573+ebfaler@users.noreply.github.com>
2021-09-19 14:39:17 +0200

    updated overlay with answer

--- js/app.js
+++ js/app.js
@@ -9,14 +9,13 @@
 
 /*MAIN VARIABLES*/
 
-const qwerty =  document.getElementById("qwerty");
-const phrase = document.getElementById("phrase");
+const qwerty = document.getElementById("qwerty");
 const phrases = [
-'Seize the day',
-'Live laugh love',
-'Only way is up',
-'Simply the best',
-'The sky is the limit'
+    'Seize the day',
+    'Live laugh love',
+    'Only way is up',
+    'Simply the best',
+    'The sky is the limit'
 
 ];
 
@@ -33,7 +32,7 @@ let heart = document.querySelectorAll('.tries img');
 // hides the overlay page when user clicks start button
 const hideOverlay = () => {
 
-overlay.style.display = "none";    
+    overlay.style.display = "none";
 
 }
 
@@ -44,17 +43,17 @@ btnReset.addEventListener('click', hideOverlay)
 
 qwerty.addEventListener('click', (e) => {
     if (e.target.tagName === 'BUTTON') {
-       e.target.className = 'chosen';
-       e.target.disabled = true;
+        e.target.className = 'chosen';
+        e.target.disabled = true;
 
-       const letterFound = checkLetter(e.target);
-       
-       if (!letterFound) {
-           heart[missed].src = 'images/lostHeart.png';
-           missed += 1;
+        const letterFound = checkLetter(e.target);
+
+        if (!letterFound) {
+            heart[missed].src = 'images/lostHeart.png';
+            missed += 1;
+        }
+        checkWin();
     }
-         checkWin();
-  }
 });
 
 
@@ -63,98 +62,108 @@ qwerty.addEventListener('click', (e) => {
 
 // selects a phrase in the array
 function getRandomPhraseAsArray(arr) {
-   
-   const randomIndex =  Math.floor(Math.random(arr) * arr.length);
-   const arrayPhrase = phrases[randomIndex];
- 
-   const newArr = arrayPhrase.split('');
-   return newArr;  
+
+    const randomIndex = Math.floor(Math.random(arr) * arr.length);
+    const arrayPhrase = phrases[randomIndex];
+
+    const newArr = arrayPhrase.split('');
+    return newArr;
 }
- const phraseArray = getRandomPhraseAsArray(phrases);
- console.log(phraseArray);
+let phraseArray = getRandomPhraseAsArray(phrases);
+console.log(phraseArray);
 
 // loops through array of characters and appends to #phrase ul to display the phrase
 function addPhraseToDisplay(arr) {
 
-   for(let i=0; i < arr.length; i++) {
-      const currentChar = arr[i];
-      const list = document.querySelector('ul');
-      const item = document.createElement('li');
-      
-      if((/[a-zA-Z]/).test(currentChar)) {
-        item.className = 'letter';
-      } else {
-        item.className = 'space';
-      };
-      item.textContent = currentChar;
-      list.append(item);
-    } 
+    for (let i = 0; i < arr.length; i++) {
+        const currentChar = arr[i];
+        const list = document.querySelector('ul');
+        const item = document.createElement('li');
+
+        if ((/[a-zA-Z]/).test(currentChar)) {
+            item.className = 'letter';
+        } else {
+            item.className = 'space';
+        };
+        item.textContent = currentChar;
+        list.append(item);
+    }
 };
 addPhraseToDisplay(phraseArray);
 
 // checkLetter This main function should loop over the letters and check if they match the letter in the button the player has chosen.
 
 function checkLetter(button) {
-    
-      const letters = document.querySelectorAll('.letter');
-      let matched = false;
 
-        for(let i=0; i < letters.length; i++) {
-        if(letters[i].textContent.toLowerCase() === button.textContent) {
-           letters[i].classList.add('show');
-           matched = true;
-        }  
-      }
-      return matched;
+    const letters = document.querySelectorAll('.letter');
+    let matched = false;
+
+    for (let i = 0; i < letters.length; i++) {
+        if (letters[i].textContent.toLowerCase() === button.textContent) {
+            letters[i].classList.add('show');
+            matched = true;
+        }
+    }
+    return matched;
 };
 
 //create a checkWin function to see if length of .letter is equal to .show class to display win/lose overlay
 
 function checkWin() {
 
-  const showClass = document.querySelectorAll(".show");
-  const letterClass = document.querySelectorAll(".letter");
-  const title = document.querySelector(".title");
+    const showClass = document.querySelectorAll(".show");
+    const letterClass = document.querySelectorAll(".letter");
+    const title = document.querySelector(".title");
 
-   if (showClass.length === letterClass.length) {
-  
-    overlay.className = "win";
-    overlay.style.display = "flex"; 
-    title.textContent = "You Won!";
-   
-};
+    if (showClass.length === letterClass.length) {
 
-   if (missed > 4) {
-   
-    overlay.className = "lose";
-    overlay.style.display = "flex"; 
-    title.textContent = "You Lost!";
-   
-};
+        overlay.className = "win";
+        overlay.style.display = "flex";
+        title.textContent = "You Won!";
 
-// recreates the buttons in the keyboard, generates a new random phrase, and sets the number of misses to zero.
+    };
 
+    if (missed > 4) {
 
-btnReset.textContent = "Reset";
-btnReset.addEventListener('click',() => {
- // clearing the list items
-    missed = 0; 
-    const list = document.querySelector('ul');
-    list.innerHTML = ``;
-    addPhraseToDisplay(getRandomPhraseAsArray(phrases));
-   // reseting the heart images
-    for (let i  =0; i < heart.length; i++) {
-        heart[i].src = 'images/liveHeart.png';
-    }
-  // reseting the keyboard and enabling the keyboard
-    const buttons = document.getElementsByTagName('button');
-       for (let i =0; i<  buttons.length; i++) {   
+        overlay.className = "lose";
+        overlay.style.display = "flex";
+        title.textContent = "You Lost!";
         
-        buttons[i].removeAttribute('disabled');
-        buttons[i].classList.remove('chosen');
+        const para = document.createElement("p");
+        title.append(para);
 
-    }
-    
+        let theText = "";
+        for (let i = 0; i < phraseArray.length; i++) {
+            theText = theText + phraseArray[i];
+        }
+
+        para.textContent = `The answer was "${theText}"`;
+        para.classList.add('answer');
+    };
+
+    // recreates the buttons in the keyboard, generates a new random phrase, and sets the number of misses to zero.
+
+    btnReset.textContent = "Reset";
+    btnReset.addEventListener('click', () => {
+        // clearing the list items
+        missed = 0;
+        const list = document.querySelector('ul');
+        list.innerHTML = ``;
+        phraseArray = getRandomPhraseAsArray(phrases);
+        addPhraseToDisplay(phraseArray);
+        // reseting the heart images
+        for (let i = 0; i < heart.length; i++) {
+            heart[i].src = 'images/liveHeart.png';
+        }
+        // reseting the keyboard and enabling the keyboard
+        const buttons = document.getElementsByTagName('button');
+        for (let i = 0; i < buttons.length; i++) {
+
+            buttons[i].removeAttribute('disabled');
+            buttons[i].classList.remove('chosen');
+
+        }
+
     });
 
 };
